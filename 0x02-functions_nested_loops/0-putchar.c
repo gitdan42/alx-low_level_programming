f5263eb668f6b000575537eaff5be4e55b316aad
gitdan42 <gitdan42@gmail.com>
2022-09-13 12:42:29 -0700

    New Fix Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,19 +7,19 @@
  * Return: always 0 for suceess
 */
 
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return(0);
+	int main(void)
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		
+		return(0);
 	
-}
+	}
 
