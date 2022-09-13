c7028e223b361fc61f67e3834b8ecd6244b69ae1
gitdan42 <gitdan42@gmail.com>
2022-09-13 12:24:16 -0700

    0-putchar.c File

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,23 @@
 #include "main.h"
 
 /**
- * This program writes a file that prints _putchar
+ * main - print putchar
+ *
+ * Return: always 0 for suceess
 */
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar('u');                                                   
-        _putchar('t');
-	_putchar('c');                                                   
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
-	_putchar('a');                      
-       	_putchar('r');                            
-	_putchar('\n');                                                   
-						       
-	return (0);
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return(0);
+	
 }
 
